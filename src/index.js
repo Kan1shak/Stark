5968fef2dc150dd11f558546f06f272dd8669a9a
Kan1shak <112748208+Kan1shak@users.noreply.github.com>
2023-10-07 14:30:54 +0530

    lower text size on mobiles

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ hamburger.addEventListener('click', () =>{
     navbar.classList.toggle('menu')
 })
 
-const featureHeading = document.querySelector('.features-heading .emphasis');
-console.log(featureHeading);
-const mediaQuery = window.matchMedia('(max-width: 1024px)');
-function handleTabletChange(e) {
-  if (e.matches) {
-    featureHeading.textContent = 'every thing';
-  }
-  else {
-    featureHeading.textContent = 'everything';
-  }
-}
+// const featureHeading = document.querySelector('.features-heading .emphasis');
+// console.log(featureHeading);
+// const mediaQuery = window.matchMedia('(max-width: 1024px)');
+// function handleTabletChange(e) {
+//   if (e.matches) {
+//     featureHeading.textContent = 'every thing';
+//   }
+//   else {
+//     featureHeading.textContent = 'everything';
+//   }
+// }
 
-mediaQuery.addListener(handleTabletChange);
-handleTabletChange(mediaQuery);
+// mediaQuery.addListener(handleTabletChange);
+// handleTabletChange(mediaQuery);
