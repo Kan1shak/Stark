9f14322d8033616dd55a4ac41e890eb9264aa823
Akshat <142678292+akshatttt321@users.noreply.github.com>
2023-10-12 23:12:04 +0530

    Add faq

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ hamburger.addEventListener('click', () =>{
     hamburgerMenu.classList.toggle('visible');
     navbar.classList.toggle('menu')
 })
+let li = document.querySelectorAll(".faq-text li");
+    for (var i = 0; i < li.length; i++) {
+      li[i].addEventListener("click", (e)=>{
+        let clickedLi;
+        if(e.target.classList.contains("question-arrow")){
+          clickedLi = e.target.parentElement;
+        }else{
+          clickedLi = e.target.parentElement.parentElement;
+        }
+       clickedLi.classList.toggle("showAnswer");
+      });
+    }
 
 // const featureHeading = document.querySelector('.features-heading .emphasis');
 // console.log(featureHeading);
